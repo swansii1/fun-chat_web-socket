f8100a8967324687460000b21f8e4f2cab8953c2
Matvey <matvey.swansii@gmail.com>
2025-04-16 17:54:54 +0300

    fear: add input and button for message

--- fun-chat/src/main/main.ts
+++ fun-chat/src/main/main.ts
@@ -8,7 +8,14 @@ export function createMain(): HTMLElement {
   const wrapperMain = createHtmlElement('div', 'wrapper_main');
   const usersContainer = createHtmlElement('div', 'users_container');
   const messageContainer = createHtmlElement('div', 'users_message');
+  const formMsg = createHtmlElement('form', 'send_form');
+  const msgInput = createHtmlElement('input', 'msg_input') as HTMLInputElement;
+  const btnSend = createHtmlElement('button', 'send_btn', 'Отправить');
 
+  msgInput.placeholder = 'Введите сообщение...';
+
+  formMsg.append(msgInput, btnSend);
+  messageContainer.append(formMsg);
   wrapperMain.append(usersContainer, messageContainer);
   main.append(wrapperMain);
   return main;
